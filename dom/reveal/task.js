fceca6a6a08fbdb3d63591247b8a041c2e7c91ed
Ilia <108480600+Volleyboler@users.noreply.github.com>
2024-09-24 22:02:54 +0500

    Update task.js

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -4,8 +4,7 @@ document.addEventListener('scroll', function() {
   reveals.forEach(reveal => {
     const rect = reveal.getBoundingClientRect();
     const windowHeight = window.innerHeight;
-
-        // Проверяем, находится ли элемент в поле зрения
+    
     if (rect.top < windowHeight && rect.bottom > 0) {
       reveal.classList.add('reveal_active');
     }
